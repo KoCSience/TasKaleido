4ac12f8417350c3aee7fda1a4152a69d5030bd02
smk7758 <18097393+smk7758@users.noreply.github.com>
2023-09-02 20:06:34 +0900

    remove security

--- src/main/kotlin/com/github/kocsience/controller/Controller.kt
+++ src/main/kotlin/com/github/kocsience/controller/Controller.kt
@@ -3,13 +3,12 @@ package com.github.kocsience.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class Controller {
     @GetMapping("/", "/greeting")
     fun hello(model: Model): String {
-        model.addAttribute("message", "HIHIHIHIHIHI");
+        model.addAttribute("message", "HIHIHIHIHIHI")
         return "index"
     }
 }
