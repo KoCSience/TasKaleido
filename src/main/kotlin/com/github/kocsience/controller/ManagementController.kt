c72c41706d1e7f9b528f9cea6e2fbb49326e4000
smk7758 <18097393+smk7758@users.noreply.github.com>
2023-09-07 14:47:02 +0900

    アカウント表示の改善

---
+++ src/main/kotlin/com/github/kocsience/controller/ManagementController.kt
@@ -0,0 +1,36 @@
+package com.github.kocsience.controller
+
+import com.github.kocsience.service.AccountService
+import com.github.kocsience.service.TaskService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("management.html")
+class ManagementController(private val accountService: AccountService, private val taskService: TaskService) {
+    @Autowired
+    lateinit var session: SessionHolder
+    @GetMapping("/css/{path:.+}")
+    fun cssPaths(@PathVariable path: String): String {
+        return "/css/$path"
+    }
+
+    @GetMapping("/js/{path:.+}")
+    fun jsPaths(@PathVariable path: String): String {
+        return "/js/$path"
+    }
+
+    @GetMapping
+    fun showManagement(model: Model): String {
+        // とりあえず仮でこれで , アカウントで分けていないけど！ｗ
+        model.addAttribute("accounts", accountService.findAll())
+
+
+        model.addAttribute("task", taskService.vanillaTask(accountService.vanillaAccount())) // 誰のtaskなのか指定できるようにする
+        return "management"
+    }
+}
