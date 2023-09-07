28d520fab39a2f8d0fdda5a2860574ddaac983ab
KoCSience <90507944+KoCSience@users.noreply.github.com>
2023-09-07 16:36:46 +0900

    fix redirect

--- src/main/kotlin/com/github/kocsience/controller/TopController.kt
+++ src/main/kotlin/com/github/kocsience/controller/TopController.kt
@@ -27,10 +27,9 @@ class TopController(private val accountService: AccountService, private val task
         return "/js/$path"
     }
 
-
-    @GetMapping("/index.html")
+    @GetMapping("", "/", "/index.html")
     fun hello(model: Model): String {
-        return demo()
+        return "redirect:demo"
     }
 
     @GetMapping("management.html")
