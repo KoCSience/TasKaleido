0aaebab5e2f6f4dfafd2aed72253ca223e33f1f7
KoCSience <90507944+KoCSience@users.noreply.github.com>
2023-09-07 18:35:11 +0900

    add Today

--- src/main/kotlin/com/github/kocsience/controller/AccountController.kt
+++ src/main/kotlin/com/github/kocsience/controller/AccountController.kt
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Controller
 @RequestMapping("/accounts", "/accounts/")
@@ -56,6 +58,7 @@ class AccountController(private val accountService: AccountService, private val 
         model.addAttribute("account", account)
         model.addAttribute("tasks", account.tasks)
         model.addAttribute("busynessStatusColor", busynessStatusColorChange(account.busynessStatus))
+        model.addAttribute("today", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
         return "accounts/subordinate"
     }
 
