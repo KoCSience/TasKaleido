b70ba048f49cf1bb56fb55b8dc53d7521fc3885e
wiwiwiwyuu <takagi.yuto.g5@s.mail.nagoya-u.ac.jp>
2023-09-08 14:58:50 +0900

    managementのレイアウト変えた

--- src/main/kotlin/com/github/kocsience/controller/ManagementController.kt
+++ src/main/kotlin/com/github/kocsience/controller/ManagementController.kt
@@ -8,6 +8,11 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 @Controller
 @RequestMapping("management.html")
@@ -31,6 +36,22 @@ class ManagementController(private val accountService: AccountService, private v
         model.addAttribute("accounts", accountService.findAll())
 
         model.addAttribute("task", taskService.vanillaTask(accountService.vanillaAccount())) // 誰のtaskなのか指定できるようにする
+        val current = LocalDateTime.now()
+        val nextSaturday: LocalDateTime = if (current.dayOfWeek == DayOfWeek.SATURDAY) {
+            current
+        } else {
+            val daysUntilSaturday = DayOfWeek.SATURDAY.value - current.dayOfWeek.value
+            current.plusDays(daysUntilSaturday.toLong())
+        }
+        val currentYearMonth = YearMonth.now()
+        val lastDayOfMonth: LocalDate = currentYearMonth.atEndOfMonth()
+        model.addAttribute("today", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")))
+        model.addAttribute(
+                "tomorrow",
+                LocalDateTime.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+        )
+        model.addAttribute("nextSaturday", nextSaturday.format(DateTimeFormatter.ofPattern("yyyy/MM/dd")))
+        model.addAttribute("endOfMonth", lastDayOfMonth.format(DateTimeFormatter.ofPattern("yyyy/MM/dd")))
         return "management"
     }
 }
